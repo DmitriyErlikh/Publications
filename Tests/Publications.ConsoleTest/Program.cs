6d7805c34eadf20c23611987dc12c5fd5dc84bf5
dmitriyerlikh <dmitriyerlikh@gmail.com>
2021-12-01 15:31:33 +0400

    HW_1

---
+++ Tests/Publications.ConsoleTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace Publications.ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var timer_tread = new Thread(() => TimerTread());
+            timer_tread.Start();
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            
+            __TimerTreadCanWork = false;
+            if (timer_tread.Join(200))
+            {
+                Console.WriteLine("Поток таймера закрыт");
+            }
+            else
+            {
+                Console.WriteLine("Поток таймера не закрыт");
+            }
+        }
+        private static bool __TimerTreadCanWork = true;
+        private static void TimerTread()
+        {
+            while (__TimerTreadCanWork)
+            {
+                var time = DateTime.Now.ToString("HH:mm:ss:ffff");
+                Console.WriteLine(time);
+                Thread.Sleep(10);
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ UI/Publications.WPF/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Publications.WPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var fib_thread = new Thread(() => Fibonacci(10));
+            fib_thread.Start();
+        }
+
+        private void Fibonacci(int number)
+        {
+            int first = 0;
+            int second = 1;
+            int sum = 0;
+            int count = 0;
+            while (count != number)
+            {
+                for (int i = 3; i >= 0; i--)
+                {
+                    Thread.Sleep(1000);
+                    TimerTextBlock.Dispatcher.Invoke(() =>
+                    {
+                        TimerTextBlock.Text = i.ToString();
+                    });
+                }
+                sum = first + second;
+                FibTextBlock.Dispatcher.Invoke(() =>
+                {
+                    FibTextBlock.Text = sum.ToString();
+                });
+                first = second;
+                second = sum;
+                count++;
+            } 
+        }
+    }
+}
